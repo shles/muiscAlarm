7fbcd4182c1493d6e66048383c2c932a047ea935
Artemiy Shlesberg <temitrix@gmail.com>
2021-01-28 12:23:18 +0300

    Initial Commit

---
+++ MusicAlarm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicAlarm
+//
+//  Created by Артeмий Шлесберг on 28.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicAlarm/MusicAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAlarmApp.swift
+//  MusicAlarm
+//
+//  Created by Артeмий Шлесберг on 28.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct MusicAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
